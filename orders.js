964f38ed01e213c1720522c9b81b3c7f520e849f
HEZRON <99868005+KialoHezz@users.noreply.github.com>
2023-03-17 17:55:19 +0300

    Change the theme and append tr child

---
+++ orders.js
@@ -0,0 +1,27 @@
+const Orders = [
+    {
+        productName: 'Foldablle Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Ruko F11',
+        productNumber: '831',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11',
+        productNumber: '3456667',
+        paymentStatus: 'due',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '12345678',
+        paymentStatus: 'paid',
+        shipping: 'delivered'
+    }
+
+]
